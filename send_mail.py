2977fe8b1428068d1b9cc9a5a09b0d01690f1375
Nestor <nestoralvaro@gmail.com>
2017-03-04 13:27:15 +0100

    Now we can also send e-mails from gmail (Google mail)

--- send_mail.py
+++ send_mail.py
@@ -4,7 +4,9 @@ import smtplib
 
 """
 description:
-    Utility for sending e-mails on python using the local SMTP server.
+    Utility for sending e-mails on python
+    You can choose between sending these e-mails using the local SMTP server, or
+        you can send the messages using GOOGLE mail SMTP server.
     Caution: These e-mails may appear on the "SPAM" folder.
 author:
     nestoralvaro
@@ -22,22 +24,6 @@ def getMailNames(mail_addrs):
     else:
       names = mail_addrs[:mail_addrs.find("@")]
     return names
-
-def sendEmail(sender, receivers, message):
-    """
-    Sends the e-mail message using the provided sender and receiver(s)
-
-    Args:
-      sender: (string) e-mail address for the sender of the e-mail
-      receivers: (list) e-mail addresses for all the recipents of the e-mail
-      msg: (string) message body for the e-mail to be sent
-    """
-    try:
-       smtpObj = smtplib.SMTP('localhost')
-       smtpObj.sendmail(sender, receivers, message)
-       print "Successfully sent email"
-    except Exception as e:
-       print "Error: unable to send email", e
 
 def buildMessage(sender, receivers, subject, msg_body):
     """
@@ -60,16 +46,59 @@ def buildMessage(sender, receivers, subject, msg_body):
         subject, msg_body)
     return message
 
+def sendEmail(sender, receivers, message):
+    """
+    Sends the e-mail message using the local SMTP server
+
+    Args:
+      sender: (string) e-mail address for the sender of the e-mail
+      receivers: (list) e-mail addresses for all the recipents of the e-mail
+      msg: (string) message body for the e-mail to be sent
+    """
+    try:
+       smtpObj = smtplib.SMTP('localhost')
+       smtpObj.sendmail(sender, receivers, message)
+       print "Successfully sent email"
+    except Exception as e:
+       print "Error: unable to send email", e
+
+def sendGoogleEmail(sender, receivers, message, google_mail_password):
+    """
+    Uses the information to send the message using GOOGLE mail
+
+    Args:
+      sender: (string) e-mail address for the sender of the e-mail.
+          This has to be a GOOGLE MAIL account
+      receivers: (list) e-mail addresses for all the recipents of the e-mail
+      msg: (string) message body for the e-mail to be sent
+    """
+    try:
+       server = smtplib.SMTP('smtp.gmail.com:587')
+       server.ehlo()
+       server.starttls()
+       # If the login fails, allow it here: https://www.google.com/settings/u/1/security/lesssecureapps
+       server.login(sender, google_mail_password)
+       server.sendmail(sender, receivers, message)
+       server.quit()
+       print "Successfully sent email"
+    except Exception as e:
+       print "Error: unable to send email", e
+
 if __name__ == '__main__':
     # Person who sends the e-mail
-    sender = "me@localhost.com"
+    sender = u"MY_OWN_EMAIL_ADDRESS@gmail.com"
     # Recipients of this e-mail (one or more)
     receivers = ["MY_FRIEND_1@gmail.com", "MY_FRIEND_2@hotmail.com"]
     # Subject text of the e-mail
-    subject = "Hola :-)"
+    subject = "Holaaaaa :-)"
     # Main body of the e-mail (can be HTML code)
     msg_body = """Hi There!  <h1>Hola</h1>\n\nCan you, <b>read this</b>\n<h2>E-mail?</h2>"""
     # Full message
     message = buildMessage(sender, receivers, subject, msg_body)
-    # Send the e-mail
-    sendEmail(sender, receivers, message)
+    # Send the e-mail using your local SMTP server
+    #sendEmail(sender, receivers, message)
+    # TODO: Put here your google mail (gmail) password
+    google_mail_password = 'YOUR_GOOGLE_MAIL_ACCOUNT_PASSWORD'
+    # Send the same e-mail using GOOGLE SMTP server.
+    # TODO: make sure the "sender" is your google mail (gmail) account.
+    sendGoogleEmail(sender, receivers, message, google_mail_password)
